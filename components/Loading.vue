69181315e51892e4746121b5e8da4a46f6ee0146
George Yankov <georgi.qnkov@gmail.com>
2021-11-12 17:50:21 +0200

    publish app

---
+++ components/Loading.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="loading">
+        <span></span>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+.loading {
+    padding-top: 120px;
+    align-items: flex-start;
+}
+</style>
